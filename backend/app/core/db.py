6fced90f7b913647855d7df66dd816653a78f4ee
bababala1994 <wuqiwen0571@163.com>
2026-05-06 16:53:32 +0800

    变更数据库初始化

--- backend/app/core/db.py
+++ backend/app/core/db.py
@@ -4,8 +4,8 @@ from app.core.config import settings
 engine = create_engine(settings.DATABASE_URL, connect_args={"check_same_thread": False})
 
 def init_db():
-    # 使用相对导入，这是包内通信最稳妥的方式
-    from ..models.repository import Repository
+    # 修正文件名：从 repository 改为 repo
+    from ..models.repo import Repository
     SQLModel.metadata.create_all(engine)
 
 def get_session():
