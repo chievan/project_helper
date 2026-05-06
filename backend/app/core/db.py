857bcb8a8ea428461e4635a47f2c2576d0a4919b
bababala1994 <wuqiwen0571@163.com>
2026-05-06 16:50:48 +0800

    更新数据库

--- backend/app/core/db.py
+++ backend/app/core/db.py
@@ -4,8 +4,8 @@ from app.core.config import settings
 engine = create_engine(settings.DATABASE_URL, connect_args={"check_same_thread": False})
 
 def init_db():
-    # 使用绝对导入，配合 PYTHONPATH=. 确保成功
-    import app.models.repository
+    # 使用相对导入，这是包内通信最稳妥的方式
+    from ..models.repository import Repository
     SQLModel.metadata.create_all(engine)
 
 def get_session():
